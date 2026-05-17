407fa96ab52e6f404de516c34d9137a28808a9cd
Radhy Shyam <radhy2929@gmail.com>
2026-05-17 16:30:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+let input_one=document.querySelector("#username");
+let btn=document.querySelector("#submit");
+let check=document.querySelector("#checkbox");
+let input_two=document.querySelector("#password");
+let exist_btn=document.querySelector("#existing");
+exist_btn.style.display="none";
+
+btn.addEventListener("click",(e)=>{
+
+	e.preventDefault();
+	alert(`Logged in as ${input_one.value}`);
+
+if(check.checked)
+{
+	localStorage.setItem(input_two.value,input_one.value);
+	exist_btn.style.display="block";
+}
+	else{
+		localStorage.removeItem("data");
+		//alert(`credentials are not stored`);
+	}
+})
+exist_btn.addEventListener("click",display)
+function display()
+{
+	let d=localStorage.getItem(input_two.value);
+	alert(`Logged in as ${d}`);
+}
